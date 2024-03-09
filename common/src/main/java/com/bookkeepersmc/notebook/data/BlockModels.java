bb70558f689a3d020af381650fa57391f35cf4ec
CrypticVerse <crypticversemc@gmail.com>
2024-03-09 11:48:09 -0500

    Model Generator

---
+++ common/src/main/java/com/bookkeepersmc/notebook/data/BlockModels.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023, 2024 BookkeepersMC under the MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.bookkeepersmc.notebook.data;
+
+import com.google.gson.JsonElement;
+import net.minecraft.data.BlockFamily;
+import net.minecraft.data.models.BlockModelGenerators;
+import net.minecraft.data.models.blockstates.BlockStateGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class BlockModels extends BlockModelGenerators {
+    @Nullable
+    private BlockFamily family;
+    private final Set<Block> children = new HashSet<>();
+
+    public BlockModels(Consumer<BlockStateGenerator> consumer, BiConsumer<ResourceLocation, Supplier<JsonElement>> biConsumer, Consumer<Item> itemConsumer) {
+        super(consumer, biConsumer, itemConsumer);
+    }
+
+    public BlockModels familyProvider(BlockFamily family) {
+        this.family = family;
+        family.getVariants().forEach((variant, block) -> {
+            if (!this.children.contains(block)) {
+                BiConsumer<BlockModels, Block> biConsumer = (BiConsumer) BlockModelGenerators.SHAPE_CONSUMERS.get(variant);
+                if (biConsumer != null) {
+                    biConsumer.accept(this, block);
+                }
+            }
+        });
+        return this;
+    }
+}
